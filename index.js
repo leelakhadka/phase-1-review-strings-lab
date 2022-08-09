bbf89c5f46f04f609facd88eb7d80dce08ebaf61
leela khadka <khadkaleela3@gmail.com>
2022-08-08 20:37:49 -0700

    completed test

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-// Write your code in this file!
+var currentUser = "The 'currentUser' variable must contain a string";
+currentUser = "You need to modify the value of the 'currentUser' variable";
+const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
+
+var excitedWelcomeMessage = 'WELCOME TO FLATBOOK, ';
+const upperCaseCurrentUser = currentUser.toUpperCase();
+excitedWelcomeMessage = `WELCOME TO FLATBOOK, ${upperCaseCurrentUser}!`;
+
+var shortGreeting = 'Welcome, ';
+const firstInitial = currentUser[0];
+const restOfName = currentUser.slice(1);
+shortGreeting = `Welcome, ${firstInitial}!`; 
