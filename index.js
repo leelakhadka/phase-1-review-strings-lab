687af6befde67f7165934ad500417ef10936d6fd
leela khadka <khadkaleela3@gmail.com>
2023-01-05 11:24:37 -0800

    commit tree 53d1745b56fb7ecbd58872f5fe9192c33529bcfb

--- index.js
+++ index.js
@@ -1,12 +1,2 @@
-var currentUser = "The 'currentUser' variable must contain a string";
-currentUser = "You need to modify the value of the 'currentUser' variable";
-const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
-
-var excitedWelcomeMessage = 'WELCOME TO FLATBOOK, ';
-const upperCaseCurrentUser = currentUser.toUpperCase();
-excitedWelcomeMessage = `WELCOME TO FLATBOOK, ${upperCaseCurrentUser}!`;
-
-var shortGreeting = 'Welcome, ';
-const firstInitial = currentUser[0];
-const restOfName = currentUser.slice(1);
-shortGreeting = `Welcome, ${firstInitial}!`; 
+var currentUser = 'Grace Hopper';
+const welcomeMessage = 'Welcome to Flatbook, ';
